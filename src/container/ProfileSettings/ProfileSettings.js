b2f63f7d3523849ecf9bbcbae0e70af07274422f
Prem Mali <premmali1835@gmail.com>
2020-10-06 18:28:46 +0530

    Profile settings fixed

--- src/container/ProfileSettings/ProfileSettings.js
+++ src/container/ProfileSettings/ProfileSettings.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { Box } from "@material-ui/core";
+import { Settings, Shield } from "react-feather";
 
 const ProfileSettings = () => {
-  return <div>Profile Settings</div>;
+  return (
+    <>
+      <Box
+        marginBottom="2rem"
+        display="flex"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <p>
+          Welcome Marissa <br />
+          Account #3241D
+        </p>
+        <Shield color="darkcyan" size={45} />
+      </Box>
+      <Box
+        display="flex"
+        justifyContent="center"
+        marginBottom="1rem"
+        marginLeft="2rem"
+      >
+        <Settings color="darkcyan" size={40} />
+      </Box>
+      <Box border="2px solid darkgrey" textAlign="center" marginLeft="2rem">
+        <p>My AVLON</p>
+        <p>Account Settings</p>
+        <p>Layout</p>
+      </Box>
+      <Box border="2px solid darkgrey" textAlign="center" marginLeft="2rem">
+        <p>Contact us</p>
+      </Box>
+    </>
+  );
 };
 
 export default ProfileSettings;
